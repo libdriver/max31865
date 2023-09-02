fde5379a97b0e2e1abec84e5bf72d29219999028
e <lishifenging@outlook.com>
2023-09-02 16:09:24 +0800

    docs: change some docs errors

--- doc/html/search/defines_2.js
+++ doc/html/search/defines_2.js
@@ -1,13 +1,13 @@
 var searchData=
 [
   ['manufacturer_5fname_256',['MANUFACTURER_NAME',['../driver__max31865_8c.html#aaa2b8f5b105c3019df0cb346f472e803',1,'driver_max31865.c']]],
-  ['max31856_5freg_5fconfig_257',['MAX31856_REG_CONFIG',['../driver__max31865_8c.html#a642c0556ea83ef6e81ff719b7ac7b863',1,'driver_max31865.c']]],
-  ['max31856_5freg_5ffault_5fstatus_258',['MAX31856_REG_FAULT_STATUS',['../driver__max31865_8c.html#a3a75e75c5ad46298d5d6a68594838cc1',1,'driver_max31865.c']]],
-  ['max31856_5freg_5fhigh_5ffault_5flsb_259',['MAX31856_REG_HIGH_FAULT_LSB',['../driver__max31865_8c.html#a79393393f2edd9ec000594624a54b49f',1,'driver_max31865.c']]],
-  ['max31856_5freg_5fhigh_5ffault_5fmsb_260',['MAX31856_REG_HIGH_FAULT_MSB',['../driver__max31865_8c.html#a2bfc93f3bf12bdc79374ae5e5980e39d',1,'driver_max31865.c']]],
-  ['max31856_5freg_5flow_5ffault_5flsb_261',['MAX31856_REG_LOW_FAULT_LSB',['../driver__max31865_8c.html#aa3b75d313d940ac09014c30dd05904d5',1,'driver_max31865.c']]],
-  ['max31856_5freg_5flow_5ffault_5fmsb_262',['MAX31856_REG_LOW_FAULT_MSB',['../driver__max31865_8c.html#a180cc4095e1891ba8c5013f6820b92a4',1,'driver_max31865.c']]],
-  ['max31856_5freg_5frtd_5flsb_263',['MAX31856_REG_RTD_LSB',['../driver__max31865_8c.html#adc655ae1f7ae768a151754e4d01ab553',1,'driver_max31865.c']]],
-  ['max31856_5freg_5frtd_5fmsb_264',['MAX31856_REG_RTD_MSB',['../driver__max31865_8c.html#a937e8286179f56c7fa61f92bb48be77b',1,'driver_max31865.c']]],
+  ['max31865_5freg_5fconfig_257',['MAX31865_REG_CONFIG',['../driver__max31865_8c.html#acb20565f5ff005379a0d4d2f9353115f',1,'driver_max31865.c']]],
+  ['max31865_5freg_5ffault_5fstatus_258',['MAX31865_REG_FAULT_STATUS',['../driver__max31865_8c.html#a19f7a992c2fddc002a2a1f85010aa140',1,'driver_max31865.c']]],
+  ['max31865_5freg_5fhigh_5ffault_5flsb_259',['MAX31865_REG_HIGH_FAULT_LSB',['../driver__max31865_8c.html#a195c07ff775d00359f90c076ba5f4df6',1,'driver_max31865.c']]],
+  ['max31865_5freg_5fhigh_5ffault_5fmsb_260',['MAX31865_REG_HIGH_FAULT_MSB',['../driver__max31865_8c.html#ab942991ee2e90df0fdc541b65847b1bc',1,'driver_max31865.c']]],
+  ['max31865_5freg_5flow_5ffault_5flsb_261',['MAX31865_REG_LOW_FAULT_LSB',['../driver__max31865_8c.html#a3de973162c39e3b5aeafabb95a5d5157',1,'driver_max31865.c']]],
+  ['max31865_5freg_5flow_5ffault_5fmsb_262',['MAX31865_REG_LOW_FAULT_MSB',['../driver__max31865_8c.html#aa5d2a148f3dd80fb0ba900ff0fa90801',1,'driver_max31865.c']]],
+  ['max31865_5freg_5frtd_5flsb_263',['MAX31865_REG_RTD_LSB',['../driver__max31865_8c.html#ab0cc6587992bcf2aca467a4169d98dd3',1,'driver_max31865.c']]],
+  ['max31865_5freg_5frtd_5fmsb_264',['MAX31865_REG_RTD_MSB',['../driver__max31865_8c.html#ad7f4dc5797a6a4d69aa592b8d8a8f991',1,'driver_max31865.c']]],
   ['max_5fcurrent_265',['MAX_CURRENT',['../driver__max31865_8c.html#a2989837a37d6d63b59c6dd541b785435',1,'driver_max31865.c']]]
 ];
